4aa472a9300b43dd43ee8d59f0d1ea7ce10e77ba
shubhankar4202 <49246868+shubhankar4202@users.noreply.github.com>
2019-04-14 23:33:33 +0530

    Create q1.c

---
+++ 14th April/q1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main (int argc , char* argv)
+{
+	int i,j;
+
+	for (i=1 ; i <= 6 ; i++)
+	{
+		for (j=1 ; j <= i ; j++)
+		{
+			printf ("*");
+		}
+	printf ("\n");
+	}
+	return 0;
+}
